d5f06a3d28f91dc721393b7129a0f9fa8a98dd16
Sylvain HENRY <hsyl20@gmail.com>
2012-05-16 15:11:32 +0200

    Platform: add wrapper to disable StarPU's dataflow mode

--- StarPU/Platform.hs
+++ StarPU/Platform.hs
@@ -3,10 +3,11 @@
 module StarPU.Platform where
 
 import StarPU.Structures
-
 import Foreign.Ptr
 import Foreign.C
- 
+
+{- StarPU's platform foreign functions -}
+
 foreign import ccall unsafe "starpu.h starpu_init" initialize :: Ptr StarPUConf -> IO CInt
 foreign import ccall unsafe "starpu.h starpu_shutdown" shutdown :: IO ()
 foreign import ccall unsafe "starpu.h starpu_worker_get_count" workerCount :: CUInt
@@ -15,24 +16,40 @@ foreign import ccall unsafe "starpu.h starpu_cpu_worker_get_count" cpuWorkerCoun
 foreign import ccall unsafe "starpu.h starpu_cuda_worker_get_count" cudaWorkerCount :: CUInt
 foreign import ccall unsafe "starpu.h starpu_spu_worker_get_count" spuWorkerCount :: CUInt
 foreign import ccall unsafe "starpu.h starpu_opencl_worker_get_count" openclWorkerCount :: CUInt
-foreign import ccall unsafe "starpu.h starpu_asynchronous_copy_disabled" starpu_asynchronous_copy_disabled :: CInt
+foreign import ccall unsafe "starpu.h starpu_asynchronous_copy_disabled" asynchronousCopyDisabled :: CInt
 
 foreign import ccall unsafe "starpu_util.h starpu_helper_cublas_init" cublasInit :: IO ()
 
-asynchronousCopyEnabled = if (starpu_asynchronous_copy_disabled == 0) then True else False
+foreign import ccall unsafe "starpu_data_set_default_sequential_consistency_flag" dataflowModeSet :: CUInt -> IO ()
+foreign import ccall unsafe "starpu_data_get_default_sequential_consistency_flag" dataflowModeGet :: CUInt
 
-defaultInit = initialize nullPtr
+{- Platform API -}
 
+{- |Indicate if asynchronous copies are enabled -}
+asynchronousCopyEnabled = (asynchronousCopyDisabled == 0)
+
+{- |Indicate if StarPU's data flow mode is enabled -}
+dataflowModeEnabled = (dataflowModeGet /= 0)
+
+{- |Initialize StarPU to be used from Haskell -}
+defaultInit = do
+  initialize nullPtr
+  dataflowModeSet 0
+
+{- |Display platform info -}
 showRuntimeInfo = putStrLn runtimeInfo
 
+{- |Return platform info -}
 runtimeInfo = foldl1 (\x y -> x ++ "\n" ++ y) infos
   where
-    infos = [workers,combinedWorkers,cpuWorkers,cudaWorkers,openclWorkers,spuWorkers,async]
+    infos = [workers,combinedWorkers,cpuWorkers,cudaWorkers,openclWorkers,spuWorkers,async,dataflow]
     workers = "There are " ++ (show workerCount) ++ " workers"
     combinedWorkers = "There are " ++ (show combinedWorkerCount) ++ " combined workers"
     cpuWorkers = "There are " ++ (show cpuWorkerCount) ++ " cpu workers"
     cudaWorkers = "There are " ++ (show cudaWorkerCount) ++ " cuda workers"
     spuWorkers = "There are " ++ (show spuWorkerCount) ++ " spu workers"
     openclWorkers = "There are " ++ (show openclWorkerCount) ++ " opencl workers"
-    async = "Asynchronous copy mechanism is " ++ (if asynchronousCopyEnabled then "enabled" else "disabled")
+    async = "Asynchronous copy mechanism is " ++ (enabled asynchronousCopyEnabled)
+    dataflow = "Dataflow mode is " ++ (enabled dataflowModeEnabled)
+    enabled x = if x then "enabled" else "disabled"
 
