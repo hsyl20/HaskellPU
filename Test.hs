8d56e2dfc9e82d7fa8b395a581f614c8c6719283
Sylvain HENRY <sylvain.henry@inria.fr>
2012-05-24 10:48:15 +0200

    Show elapsed time

--- Test.hs
+++ Test.hs
@@ -2,6 +2,7 @@ import Prelude hiding (mapM,foldl1)
 import Data.Ix
 import Data.Traversable
 import Data.Foldable
+import Data.Time.Clock
 
 import StarPU.Platform
 import StarPU.DataTypes
@@ -12,7 +13,8 @@ import BLAS
 import QR
 import HighDataTypes
 
-n = 16
+n = 4096
+nb = 4
 
 m1 = floatMatrixInit (\x y -> 1.0) n n
 m2 = floatMatrixInit (\x y -> if (x == y) then 1.0 else 0.0) n n
@@ -22,20 +24,28 @@ m4 = floatMatrixInit (\x y -> 3.0) n n
 m5 = floatMatrixInit (\x y -> if (x == y) then 1.0 else 0.0) 10 10
 m6 = floatMatrixInit (\x y -> fromIntegral (x + y)) 8 10
 
-ms = map createMatrix $ range (1, 96)
+ms = map createMatrix $ range (1, 30)
   where
     createMatrix v = floatMatrixInit (\x y -> fromIntegral v) n n
 
 
 main = do
-  putStrLn "Initializing..."
+  putStrLn "Initializing runtime system..."
+  t0 <- getCurrentTime
   defaultInit
   cublasInit
   showRuntimeInfo
 
-  putStrLn "Computing..."
+  putStrLn "Initializing data..."
+  t1 <- getCurrentTime
+--  mapM compute ms
+  mapM compute [m1,m2,m3,m4,m5,m6]
 
-  printFloatMatrix $ sgemm m2 m3
+  putStrLn "Computing..."
+  
+  t2 <- getCurrentTime
+
+{-  printFloatMatrix $ sgemm m2 m3
 
   putStrLn "A"
   printFloatMatrix m5
@@ -54,7 +64,14 @@ main = do
 
   putStrLn "A * B (using blocks)"
   printHighMatrix $ highSGEMM (split 1 2 m5) (split 2 1 m6)
+-}
 
+  traverseHighMatrix compute  $ highSGEMM (split nb nb m3) (split nb nb m4)
+--  compute $ reduce sgemm ms
+
+  t3 <- getCurrentTime
+  putStrLn "Done."
+  
   putStrLn "Unregister matrices..."
   unregisterInvalid m1
   unregisterInvalid m2
@@ -65,6 +82,13 @@ main = do
 
   putStrLn "Shutting down..."
   shutdown
+
+  putStrLn "==============================================================="
+  putStrLn $ "Runtime system initialisation time: " ++ show (diffUTCTime t1 t0)
+  putStrLn $ "Data initialisation time: " ++ show (diffUTCTime t2 t1)
+  putStrLn $ "Computing time: " ++ show (diffUTCTime t3 t2)
+  putStrLn "==============================================================="
+
 
 
 
