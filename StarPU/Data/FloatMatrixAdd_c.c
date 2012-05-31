993f5beb597cc0f70c2445903b0ad166f90dad88
Sylvain HENRY <sylvain.henry@inria.fr>
2012-05-31 11:32:13 +0200

    Add CPU kernels for float matrix add/sub

--- StarPU/Data/FloatMatrixAdd_c.c
+++ StarPU/Data/FloatMatrixAdd_c.c
@@ -28,12 +28,29 @@ static void matadd_cuda(void *descr[], void *_args) {
 }
 
 static void matadd_cpu(void *descr[], void *_args) {
+  float *a = (float *)STARPU_MATRIX_GET_PTR(descr[0]);
+  float *b = (float *)STARPU_MATRIX_GET_PTR(descr[1]);
+  float *c = (float *)STARPU_MATRIX_GET_PTR(descr[2]);
+
+  unsigned w = STARPU_MATRIX_GET_NX(descr[0]);
+  unsigned h = STARPU_MATRIX_GET_NY(descr[1]);
+
+  unsigned lda = STARPU_MATRIX_GET_LD(descr[0]);
+  unsigned ldb = STARPU_MATRIX_GET_LD(descr[1]);
+  unsigned ldc = STARPU_MATRIX_GET_LD(descr[2]);
+
+  unsigned i,j;
+  for (j=0; j<h; j++) {
+    for (i=0; i<w; i++) {
+      c[j*ldc+i] = a[j*lda+i] + b[j*ldb+i];
+    }
+  }
 }
 
 static struct starpu_codelet matadd_codelet =
 {
   .modes = { STARPU_R, STARPU_R, STARPU_W },
-  .where = STARPU_CUDA,
+  .where = STARPU_CUDA | STARPU_CPU,
   .cpu_funcs = {matadd_cpu, NULL},
   .cuda_funcs = {matadd_cuda, NULL},
   .nbuffers = 3,
