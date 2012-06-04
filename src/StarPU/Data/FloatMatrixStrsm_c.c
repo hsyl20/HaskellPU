80d411340d6293bcf69540d11e010c4bf16947db
Sylvain HENRY <hsyl20@gmail.com>
2012-06-05 01:11:47 +0200

    Add CPU BLAS for STRSM

--- src/StarPU/Data/FloatMatrixStrsm_c.c
+++ src/StarPU/Data/FloatMatrixStrsm_c.c
@@ -6,6 +6,11 @@
 #include "../Platform.h"
 #include "FloatMatrix_kernels.h"
 
+extern void strsm_ (const char *side, const char *uplo, const char *transa, 
+                   const char *diag, const int *m, const int *n,
+                   const float *alpha, const float *A, const int *lda,
+                   float *B, const int *ldb);
+
 static struct starpu_perfmodel strsm_model =
 {
   .type = STARPU_HISTORY_BASED,
@@ -36,7 +41,7 @@ static void strsm_cuda(void *descr[], void *args) {
   cublasSideMode_t side = arg->side ? CUBLAS_SIDE_RIGHT : CUBLAS_SIDE_LEFT;
   cublasFillMode_t uplo = arg->uplo ? CUBLAS_FILL_MODE_LOWER : CUBLAS_FILL_MODE_UPPER;
   cublasDiagType_t diag = arg->unit ? CUBLAS_DIAG_UNIT : CUBLAS_DIAG_NON_UNIT;
-  cublasOperation_t trans = CUBLAS_OP_T;
+  cublasOperation_t trans = CUBLAS_OP_N;
 
   cublasSetStream(cublas_handle, starpu_cuda_get_local_stream());
 
@@ -50,13 +55,40 @@ static void strsm_cuda(void *descr[], void *args) {
   free(arg);
 }
 
-static void strsm_cpu(void *descr[], void *_args) {
+static void strsm_cpu(void *descr[], void *args) {
+  float *a = (float *)STARPU_MATRIX_GET_PTR(descr[0]);
+  float *b = (float *)STARPU_MATRIX_GET_PTR(descr[1]);
+  float *x = (float *)STARPU_MATRIX_GET_PTR(descr[2]);
+
+  unsigned w = STARPU_MATRIX_GET_NY(descr[0]);
+  unsigned h = STARPU_MATRIX_GET_NX(descr[1]);
+
+  unsigned lda = STARPU_MATRIX_GET_LD(descr[0]);
+  unsigned ldb = STARPU_MATRIX_GET_LD(descr[1]);
+  unsigned ldx = STARPU_MATRIX_GET_LD(descr[2]);
+  
+  struct strsm_arg * arg = (struct strsm_arg *)args;
+
+  const char side = arg->side ? 'R' : 'L';
+  const char uplo = arg->uplo ? 'L' : 'U';
+  const char diag = arg->unit ? 'U' : 'N';
+  const char trans = 'N';
+
+  unsigned i;
+  for (i = 0; i < w; i++) {
+    memcpy(&x[i*ldx], &b[i*ldb], h*sizeof(float));
+  }
+
+  const float factor = 1.0f;
+  strsm_(&side, &uplo, &trans, &diag, (int*)&w, (int*)&h, &factor, a, (int*)&lda, x, (int*)&ldx);
+
+  free(arg);
 }
 
 static struct starpu_codelet strsm_codelet =
 {
   .modes = { STARPU_R, STARPU_R, STARPU_W },
-  .where = STARPU_CUDA,
+  .where = STARPU_CUDA | STARPU_CPU,
   .cpu_funcs = {strsm_cpu, NULL},
   .cuda_funcs = {strsm_cuda, NULL},
   .nbuffers = 3,
